55ed5619d94cb1e58cb89c8f90485e1d516887d7
Zsombor Hollay-Horvath <hbotondzs@gmail.com>
2017-12-13 19:51:51 +0000

    Library added, tests are missing

---
+++ src/sbx-parse-api.js
@@ -0,0 +1,123 @@
+'use strict';
+
+import {encode as b64encode} from 'base-64';
+
+const AuthMethods = {
+    BASIC: 'Basic',
+    JWT: 'Bearer',
+}
+
+class Parse
+{
+    constructor(url, authMethod, ...args)
+    {
+        let auth;
+        switch (authMethod) {
+            case AuthMethods.BASIC:
+                if (args.length === 2) {
+                    auth = b64encode(`${args[0]}:${args[1]}`);
+                } else {
+                    throw new Error('Username and password must not be empty');
+                }
+            break;
+            case AuthMethods.JWT:
+                if (args.length === 1) {
+                    auth = args[0];
+                } else {
+                    throw new Error('JWT must not be empty');
+                }
+            break;
+            default:
+                throw new Error(`Invalid auth type "${authMethod}"`);
+            //
+        }
+
+        this.url = url;
+        this.headers = new Headers();
+        this.headers.append('Authorization', `${authMethod} ${auth}`);
+        this.headers.append('Content-Type', 'application/json');
+        this.headers.append('Accept', 'application/json');
+    }
+
+    _create_payload(docs)
+    {
+        let result = [];
+
+        for (let doc of docs) {
+            let obj = {};
+
+            if (doc.buffer !== undefined) {
+                obj['content'] = btoa(
+                    String.fromCharCode(...new Uint8Array(doc.buffer)));
+            } else if (doc.bytes !== undefined) {
+                obj['content'] = b64encode(obj.bytes);
+            } else if (doc.content !== undefined) {
+                obj['content'] = obj.content;
+            } else {
+                throw Error('No document content found!');
+            }
+
+            if (doc.id !== undefined) {
+                obj['id'] = doc.id;
+            }
+
+            if (doc.labelId !== undefined) {
+                obj['labelId'] = doc.labelId;
+            }
+
+            if (doc.detailedLabelId !== undefined) {
+                obj['detailedLabelId'] = doc.id;
+            }
+
+            result.push(obj);
+        }
+
+        return JSON.stringify(result);
+    }
+
+    _request(docs, url)
+    {
+        return fetch(
+            `${this.url}${url}`, {
+                    method:'POST',
+                    headers: this.headers,
+                body: this._create_payload(docs),
+                credentials: 'include'
+                })
+        .then(function(response) {
+            if (!response.ok) {
+                throw {'error': response};
+            }
+            return response.json();
+        })
+        .catch(function(error) {
+            throw {'error': error}
+        });
+    }
+
+    classify(docs, take)
+    {
+        let url = '/docs/classify';
+        if (take !== undefined) {
+            url += `?take=${take}`;
+        }
+        return this._request(docs, url);
+    }
+
+    parse(docs, mode)
+    {
+        let url = '/docs/parse';
+        if (mode !== undefined) {
+            url += `?mode=${mode}`;
+        }
+        return this._request(docs, url);
+    }
+
+    feed(docs)
+    {
+        let url = '/docs/feed';
+        return this._request(docs, url);
+    }
+}
+
+export {Parse, AuthMethods};
